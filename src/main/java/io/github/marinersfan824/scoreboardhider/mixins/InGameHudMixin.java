3d4c7b8bb6f7547e324a4346af4d3650fb113aea
marinersfan824 <70529932+marinersfan824@users.noreply.github.com>
2023-07-21 13:28:23 -0700

    removed title + subtitle text

--- src/main/java/io/github/marinersfan824/scoreboardhider/mixins/InGameHudMixin.java
+++ src/main/java/io/github/marinersfan824/scoreboardhider/mixins/InGameHudMixin.java
@@ -3,6 +3,7 @@ package io.github.marinersfan824.scoreboardhider.mixins;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,6 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class InGameHudMixin {
     @Inject(method = "renderScoreboardSidebar", at = @At("HEAD"), cancellable = true)
     private void cancelSidebar(MatrixStack matrices, ScoreboardObjective objective, CallbackInfo ci) {
+        ci.cancel();
+    }
+
+    @Inject(method = "setTitle", at = @At("HEAD"), cancellable = true)
+    private void cancelTitle(Text title, CallbackInfo ci) {
         ci.cancel();
     }
 }
